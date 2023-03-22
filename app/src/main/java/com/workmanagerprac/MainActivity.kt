6976d95383eb27e5ccd3bf52f9f76a412eebe2b4
Yashveer singh <yashveersingh30401@gmail.com>
2023-03-22 18:51:09 +0530

    1. SetDelay

--- app/src/main/java/com/workmanagerprac/MainActivity.kt
+++ app/src/main/java/com/workmanagerprac/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.workmanagerprac
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
 import com.workmanagerprac.databinding.ActivityMainBinding
 import com.workmanagerprac.workManager.logWorker
+import java.time.Duration
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        val logWorkRequest:WorkRequest = OneTimeWorkRequestBuilder<logWorker>().build()
+        val logWorkRequest:WorkRequest = OneTimeWorkRequestBuilder<logWorker>()
+            .setInitialDelay(Duration.ofSeconds(5))
+            .build()
 
         WorkManager.getInstance(this).enqueue(logWorkRequest)
 
